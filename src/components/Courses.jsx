616f13b884a84be1f5ffbadaeadc5699fb301796
ashishbahar <ashishbahar987@gmail.com>
2024-01-30 12:38:21 +0530

    push

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -2,19 +2,30 @@ import React from "react";
 import img1 from "../assets/images/png/img1.png";
 import img2 from "../assets/images/png/img2.png";
 import img3 from "../assets/images/png/img3.png";
+import { CourseData } from "./Helper";
 const Courses = () => {
   return (
-    <div className="bg-black">
-      <div className="container py-5">
+    <div className="bg-black pt-5">
+      <div className="container py-5 mt-5">
         <h2 className="text-white fs-1 fw-semibold">
           Latest <span className="linear_gradient">Courses</span>
         </h2>
         <div className="row pt-5">
-          <div className="col-lg-4">
-            <div className="course_box">
-              <img src={img1} alt="img1" />
-            </div>
-          </div>
+          {CourseData &&
+            CourseData.map((obj, i) => {
+              return (
+                <div key={i} className="col-lg-4 mt-4">
+                  <div className="course_box p-3">
+                    <img className=" w-100 py-3" src={obj.img} alt="img1" />
+                    <h2 className=" text-white fs-3">{obj.h1}</h2>
+                    <p className=" text-white">{obj.p}</p>
+                    <button className=" button w-100 border-1 py-2">
+                      Enrolled
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
---
+++ src/components/Helper.jsx
@@ -0,0 +1,36 @@
+import img1 from "../assets/images/png/img1.png";
+import img2 from "../assets/images/png/img2.png";
+import img3 from "../assets/images/png/img3.png";
+
+export const CourseData = [
+  {
+    img: img1,
+    h1: "Front-End Development:",
+    p: " We communicated with over 472 founders of failed or closed startups across the globe and asked about their harrowing",
+  },
+  {
+    img: img2,
+    h1: " App Development",
+    p: " We communicated with over 472 founders of failed or closed startups across the globe and asked about their harrowing",
+  },
+  {
+    img: img3,
+    h1: "Back-End Development:",
+    p: " We communicated with over 472 founders of failed or closed startups across the globe and asked about their harrowing",
+  },
+  {
+    img: img2,
+    h1: "ui ux designer",
+    p: " We communicated with over 472 founders of failed or closed startups across the globe and asked about their harrowing",
+  },
+  {
+    img: img1,
+    h1: " App Development",
+    p: " We communicated with over 472 founders of failed or closed startups across the globe and asked about their harrowing",
+  },
+  {
+    img: img3,
+    h1: "Back-End Development:",
+    p: " We communicated with over 472 founders of failed or closed startups across the globe and asked about their harrowing",
+  },
+];
